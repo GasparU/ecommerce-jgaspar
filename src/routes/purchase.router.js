2c47dd15c3451b4471c692073889aee8bb1eafd8
Jose Gaspar <sainc2016@gmail.com>
2023-06-30 08:50:08 -0500

    modificando nombre de vidityJWT a verifyJWT

--- src/routes/purchase.router.js
+++ src/routes/purchase.router.js
@@ -1,11 +1,11 @@
 const { getAll, create } = require('../controllers/purchase.controller');
 const express = require('express');
-const veridyJWT = require('../utils/verifyJWT')
+const verifyJWT = require('../utils/verifyJWT')
 
 const routerPurchase = express.Router();
 
 routerPurchase.route('/')
-    .get(veridyJWT, getAll)
-    .post(veridyJWT, create);
+    .get(verifyJWT, getAll)
+    .post(verifyJWT, create);
 
 module.exports = routerPurchase;
