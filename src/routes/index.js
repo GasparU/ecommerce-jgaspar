190379cb9a8ba5fea9144756769a6a93452b5c0d
Jose Gaspar <sainc2016@gmail.com>
2023-07-05 19:58:27 -0500

    agregando modificaciones

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,15 @@ const routerCart = require('./cart.router');
 const routerPurchase = require('./purchase.router');
 const routerProductImg = require('./productImg.router');
 const router = express.Router();
+const verifyJWT = require('../utils/verifyJWT')
 
 // colocar las rutas aquí
 router.use('/users', routerUser)
 router.use('/categories', routerCategory)
 router.use('/products', routerProduct)
-router.use('/cart', routerCart)
-router.use('/purchase', routerPurchase)
+// si todas las peticiones de routerCart (get, post, update, delete) estan protegidas, no hace falta proteger una por una, se protege la principal como aquí con routerCart y routerPurchase.
+router.use('/cart', verifyJWT, routerCart)
+router.use('/purchase', verifyJWT, routerPurchase)
 router.use('/product_images', routerProductImg)
 
 
